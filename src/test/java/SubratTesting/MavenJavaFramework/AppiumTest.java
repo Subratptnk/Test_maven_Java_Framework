9620fb2e55e95d421377e95367307776597024f9
Subs <subratpattanaik20@gmail.com>
2022-06-08 23:11:30 +0530

    Maven_Update_01: Skeleton prepared

---
+++ src/test/java/SubratTesting/MavenJavaFramework/AppiumTest.java
@@ -0,0 +1,15 @@
+package SubratTesting.MavenJavaFramework;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAndroid() {
+		System.out.println("nativeAndroid");
+	}
+	
+	@Test
+	public void iosApps() {
+		System.out.println("iosApps");
+	}
+}
